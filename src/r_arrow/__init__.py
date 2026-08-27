6ca0839da2cd802db013f852ea044d21300d986a
kosukeya <189577816+kosukeya@users.noreply.github.com>
2026-08-27 12:45:01 +0900

    Stage 2: expose coarse-graining API

--- src/r_arrow/__init__.py
+++ src/r_arrow/__init__.py
@@ -1,6 +1,15 @@
 """r-arrow finite stochastic-process tools."""
 
-from .benchmarks import biased_three_cycle, reversible_three_cycle
+from .benchmarks import biased_four_cycle, biased_three_cycle, reversible_three_cycle
+from .coarse_grain import (
+    declared_partitions,
+    is_strongly_lumpable,
+    observed_arrow_strength,
+    observed_path_distribution,
+    partition_label,
+    robustness_ratio,
+    set_partitions,
+)
 from .irreversibility import arrow_strength, biased_cycle_analytic_arrow
 from .markov import (
     detailed_balance_holds,
@@ -13,13 +22,21 @@ from .trajectories import path_distribution, path_probability, reverse_path
 __all__ = [
     "arrow_strength",
     "biased_cycle_analytic_arrow",
+    "biased_four_cycle",
     "biased_three_cycle",
+    "declared_partitions",
     "detailed_balance_holds",
+    "is_strongly_lumpable",
+    "observed_arrow_strength",
+    "observed_path_distribution",
+    "partition_label",
     "path_distribution",
     "path_probability",
     "probability_current",
     "reverse_path",
     "reversible_three_cycle",
+    "robustness_ratio",
+    "set_partitions",
     "stationary_distribution",
     "validate_transition_matrix",
 ]
